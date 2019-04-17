690491520e3fd5a347d038fb57fb9078f8c70c9e
tramm <ram.tangirala@gmail.com>
2019-04-17 16:32:34 +0530

    Changes to auth and mongo many to on relationships

--- web/server/api/public.js
+++ web/server/api/public.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 
 const MUser = require('../models/MUser');
 const Offer = require('../models/Offer');
-const Service = require('../models/Service');
+const categories = require('../models/ServiceCategory');
 const logger = require('../logs');
 const router = express.Router();
 
@@ -33,6 +33,7 @@ router.post('/user/add', async (req, res) => {
 
 router.get('/offers', async (req, res) => {
   try {
+    console.log(Offer);
     const books = await Offer.list();
     res.json(books);
   } catch (err) {
@@ -52,92 +53,37 @@ router.post('/offers/add', async (req, res) => {
     res.json({ error: err.message || err.toString() });
   }
 });
+router.get('/offers/list', async (req, res) => {
+  try {
+    const offerList = await Offer.listByCategory(req.body);
+    console.log("In public API");
+    console.log(offerList);
+    res.json(offerList);
+  } catch (err) {
+    res.json({ error: err.message || err.toString() });
+  }
+});
 
 router.get('/services', async (req, res) => {
-    try {
-      const services = await Service.list();
-      res.json(services);
-    } catch (err) {
-      res.json({ error: err.message || err.toString() });
-    }
-  });
+  try {
+    const services = await categories.list();
+    res.json(services);
+  } catch (err) {
+    res.json({ error: err.message || err.toString() });
+  }
+});
 
-// router.get('/books/:slug', async (req, res) => {
-//   try {
-//     const book = await Book.getBySlug({ slug: req.params.slug });
-//     res.json(book);
-//   } catch (err) {
-//     res.json({ error: err.message || err.toString() });
-//   }
-// });
-
-// router.get('/get-chapter-detail', async (req, res) => {
-//   try {
-//     const { bookSlug, chapterSlug } = req.query;
-//     const chapter = await Chapter.getBySlug({
-//       bookSlug,
-//       chapterSlug,
-//       userId: req.user && req.user.id,
-//       isAdmin: req.user && req.user.isAdmin,
-//     });
-//     res.json(chapter);
-//   } catch (err) {
-//     res.json({ error: err.message || err.toString() });
-//   }
-// });
-
-// router.get('/get-table-of-contents', async (req, res) => {
-//   try {
-//     const book = await Book.findOne({ slug: req.query.slug }, 'id');
-//     if (!book) {
-//       throw new Error('Not found');
-//     }
-
-//     const chapters = await Chapter.find(
-//       { bookId: book.id, order: { $gt: 1 } },
-//       'sections title slug',
-//     ).sort({ order: 1 });
-
-//     res.json(chapters);
-//   } catch (err) {
-//     res.json({ error: err.message || err.toString() });
-//   }
-// });
-
-// router.get('/get-book-reviews', async (req, res) => {
-//   try {
-//     const reviewDoc = await Review.findOne({ bookSlug: req.query.slug }, 'reviews').lean();
-//     const reviews = _.sortBy(reviewDoc.reviews, 'order');
-//     res.json(reviews);
-//   } catch (err) {
-//     res.json({ error: err.message || err.toString() });
-//   }
-// });
-
-// router.get('/get-tutorials', async (req, res) => {
-//   try {
-//     const tutorialDoc = await Tutorial.findOne({ bookSlug: req.query.slug }, 'tutorials').lean();
-//     const tutorials = _.sortBy(tutorialDoc.tutorials, 'order');
-//     res.json(tutorials);
-//   } catch (err) {
-//     res.json({ error: err.message || err.toString() });
-//   }
-// });
-
-// router.post('/subscribe-to-tutorials', async (req, res) => {
-//   const { email } = req.body;
-//   if (!email) {
-//     res.json({ error: 'Email is required' });
-//     return;
-//   }
-
-//   try {
-//     await subscribe({ email, listName: 'tutorials' });
-//     res.json({ subscribed: 1 });
-//   } catch (err) {
-//     res.json({ error: err.message || err.toString() });
-//   }
-// });
-
+router.post('/services/add', async (req, res) => {
+  try {
+    let val = Object.assign({ userId: 1239 }, req.body);
+    console.log(val);
+    console.log(req);
+    const offer = await categories.add(req.body);
+    res.json(offer);
+  } catch (err) {
+    logger.error(err);
+    res.json({ error: err.message || err.toString() });
+  }
+});
 
 module.exports = router;
---
+++ web/server/models/ServiceCategory.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-use-before-define */
+
+const mongoose = require('mongoose');
+const logger = require('../logs');
+const { Schema } = mongoose;
+
+const mongoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    }
+});
+
+class ServiceCategoryClass {
+    static async list({ offset = 0, limit = 10 } = {}) {
+        const categories = await this.find({})
+            .sort({ createdAt: -1 })
+            .skip(offset)
+            .limit(limit).lean();
+        return { categories };
+    }
+    static async add({ name, description, url }) {
+        return this.create({
+            name,
+            description,
+            url,
+            createdAt: new Date()
+        });
+    }
+}
+mongoSchema.loadClass(ServiceCategoryClass);
+
+const ServiceCategory = mongoose.model('ServiceCategory', mongoSchema);
+
+module.exports = ServiceCategory;
+
