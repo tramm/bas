6774462db26ad7a17cece88dfa770b08d0a3b767
Anish Konda <anishkonda123@gmail.com>
2019-04-22 16:12:05 +0530

    added booking leads and partner models

---
+++ web/server/models/Patner.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-use-before-define */
+
+const mongoose = require('mongoose');
+const logger = require('../logs');
+require('dotenv').config();
+const STATIC_HOST = process.env.STATIC_WEB_HOST;
+const { Schema } = mongoose;
+
+const mongoSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  pin: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  street1: {
+    type: String,
+    required: true,
+  },
+  street2: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  }
+});
+
+class PatnerClass {
+  static async list({ offset = 0, limit = 10 } = {}) {
+    const patners = await this.find({})
+      .sort({ active: -1 })
+      .skip(offset)
+      .limit(limit);
+    return { "url":STATIC_HOST,"patners":patners };
+  }
+  static async add({ name, mobile, pin, email, street1, street2, city, zip}) {
+    if (mobile){
+        const user = await this.findOne({ mobile });
+        if(user)return user;
+        const patner =  this.create({
+            name,
+            mobile,
+            pin,
+            email,
+            street1,
+            street2,
+            city,
+            zip,
+        });
+     return patner;
+    }else {
+        console.log("ERROR in request - no mobile");
+        throw new Error('User cannot be created without mobile number');
+    }
+  }
+}
+mongoSchema.loadClass(PatnerClass);
+
+const Patner = mongoose.model('Patner', mongoSchema);
+
+module.exports = Patner;
+
